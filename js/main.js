629f440ac44545461da01ee2ce7146396c14b7e3
shanmolina <shan.nm@hotmail.es>
2022-07-17 21:05:40 -0300

    cambios en js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const menuBtn = document.querySelector('#menu-button');
-const menuItems = document.querySelectorAll('menu-item');
+const menuItems = document.querySelectorAll(".menu-item");
 
 menuBtn.addEventListener('click', function() {
     document.body.classList.toggle('mobile-menu-active');
 });
 
 menuItems.forEach(function(item) {
-
+      
     item.addEventListener('click', function() {
-        
-        document.body.classList.remove('mobile-menu-active');
+     
+      document.body.classList.remove('mobile-menu-active');
 
-        const currentItem = document.querySelector('.active');
-        currentItem.classList.remove('active');
-        this.classList.add('active');
-        });
+      const currentItem = document.querySelector('.active');
+      currentItem.classList.remove('active');
+      this.classList.add('active');
+    });
 });
